e63a1ba11f9d76ddeac8cabfc66bd28ccfbb1fcf
mobin <moobinislam3@gmail.com>
2025-06-02 13:11:01 +0600

    0

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:MyLiveClassDailyProject/moduel_11/class_2/calculetor.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DailyProject(),
+    );
+  }
+}
---
+++ lib/moduel_11/class_2/customElevatedButton.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class cutomElevatedButton extends StatelessWidget {
+  const cutomElevatedButton(
+      {super.key,
+      required this.text,
+      required this.onepress,
+      this.bgcolor,
+      this.fgcolor,
+      this.curculerboder,
+      this.width,
+      this.elvevation,
+      this.textpadin,
+      this.fontsiz});
+  final String text;
+  final VoidCallback onepress;
+  final bgcolor;
+  final fgcolor;
+  final curculerboder;
+  final width;
+  final elvevation;
+  final double? textpadin;
+  final double? fontsiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(4.0),
+        child: ElevatedButton(
+          style: cusomButtonStyle(
+            bgcolor: bgcolor,
+            fgcolor: fgcolor,
+            elvevation: elvevation,
+            curculerboder: curculerboder,
+            width: width,
+          ),
+          onPressed: onepress,
+          child: Padding(
+            padding: EdgeInsets.all(textpadin ?? 16.0),
+            child: Text(
+              textAlign: TextAlign.center,
+              text,
+              style: TextStyle(fontSize: fontsiz ?? 18),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+ButtonStyle cusomButtonStyle(
+    {required Color? bgcolor,
+    required Color? fgcolor,
+    required double? curculerboder,
+    required double? width,
+    required double? elvevation}) {
+  return ElevatedButton.styleFrom(
+    alignment: Alignment.center,
+    backgroundColor: bgcolor ?? Colors.black,
+    foregroundColor: fgcolor ?? Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(curculerboder ?? 6),
+      side: BorderSide(width: width ?? 0),
+    ),
+    elevation: elvevation ?? 0,
+  );
+}
