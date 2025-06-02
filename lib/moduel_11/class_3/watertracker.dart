5d173dfb1a8b1262c1fbde30bad26028f33c6bae
mobin <moobinislam3@gmail.com>
2025-06-02 18:13:02 +0600

    class3

---
+++ lib/moduel_11/class_3/watertracker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Watertracker extends StatefulWidget {
+  const Watertracker({super.key});
+
+  @override
+  State<Watertracker> createState() => _WatertrackerState();
+}
+
+class _WatertrackerState extends State<Watertracker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
